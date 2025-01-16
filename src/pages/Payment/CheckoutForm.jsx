ab39f2032c75ea4d582e7f7fabb39f1447870534
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-16 22:12:02 +0600

    payment gatway work running

---
+++ src/pages/Payment/CheckoutForm.jsx
@@ -0,0 +1,46 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React from 'react';
+
+const CheckoutForm = () => {
+
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const handleSubmit = async(event) =>{
+		event.preventDefault();
+
+		if(!stripe || !elements){
+			return;
+		}
+
+		const card = elements.getElement(CardElement);
+
+		if(card === null){
+			return;
+		}
+	}
+	return (
+		<div>
+			<form onSubmit={handleSubmit} action="">
+			<CardElement
+				options={{
+					style: {
+					base: {
+						fontSize: '16px',
+						color: '#424770',
+						'::placeholder': {
+						color: '#aab7c4',
+						},
+					},
+					invalid: {
+						color: '#9e2146',
+					},
+					},
+				}}
+				/>
+			</form>
+		</div>
+	);
+};
+
+export default CheckoutForm;
--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,9 +1,15 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 
+//todo: add publishable key
+const stripePromise = loadStripe('')
 const Payment = () => {
 	return (
 		<div>
-			payment
+			<Elements stripe={stripePromise}>
+
+			</Elements>
 		</div>
 	);
 };
