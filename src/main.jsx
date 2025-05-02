6014991a5ad2735a7d89874517f142f4f1dd2173
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-05-02 19:33:22 +0600

    remove: main jsx component remove unused code

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,4 @@
 
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import './index.css'
-// import { RouterProvider} from "react-router-dom";
-// import { router } from './Routes/Routes';
-// import { HelmetProvider } from 'react-helmet-async';
-// import AuthProvider from './providers/AuthProvider';
-// import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-// import { Toaster } from 'react-hot-toast';
-// import ThemeProvider from './context/ThemeContext';
-
-// const queryClient = new QueryClient();
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Toaster position="top-center" reverseOrder={false} />
-//     <AuthProvider>
-//       <QueryClientProvider client={queryClient}>
-//         <HelmetProvider>
-//          <ThemeProvider>
-//          <div className=''>
-//             <RouterProvider router={router} />
-//           </div>
-//          </ThemeProvider>
-//         </HelmetProvider>
-//       </QueryClientProvider>
-//     </AuthProvider>
-//   </React.StrictMode>,
-// )
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
